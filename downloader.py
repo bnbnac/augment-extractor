3020922793e744fa0d2a302f712d139d025eaafb
bnbnac <bnbnac@naver.com>
2023-06-09 23:30:44 +0900

    fix: need video quality at least 360p for tesseract to recognize the letters

--- downloader.py
+++ downloader.py
@@ -4,7 +4,7 @@ from yt_dlp import YoutubeDL
 def download(url, question_id):
     ydl_opts = {
         'outtmpl': 'downloads/{}.%(ext)s'.format(question_id),
-        'format': 'worstvideo'
+        'format': 'worstvideo[height>=360]'
     }
 
     with YoutubeDL(ydl_opts) as ydl:
