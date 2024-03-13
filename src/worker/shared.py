e18d772f04f2a22ab33b9c3b5e4db8a2726c5810
bnbnac <bnbnac@naver.com>
2024-03-13 12:02:49 +0900

    feat: introduce Cur_Process class and share an instance to get information about currently processing job from the process_queue

--- src/worker/shared.py
+++ src/worker/shared.py
@@ -1,3 +1,22 @@
 from queue import Queue
 
 process_queue = Queue()
+
+
+class Cur_Process:
+    def __init__(self):
+        self.post_id = ''
+        self.state = 'standby'
+        self.cur_frame = 0
+        self.total_frame = 0
+        self.quit_flag = 0
+
+    def done(self):
+        self.post_id = ''
+        self.state = 'standby'
+        self.cur_frame = 0
+        self.total_frame = 0
+        self.quit_flag = 0
+
+
+current_processing_info = Cur_Process()
